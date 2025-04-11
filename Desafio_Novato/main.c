48cbae8b0b3c61b53972eec2b252ebd70429fd5f
Thalyel <thalielb3@gmail.com>
2025-04-11 05:26:22 -0300

    Batalha Naval - Novato

--- Desafio_Novato/main.c
+++ Desafio_Novato/main.c
@@ -1,72 +1,125 @@
 #include <stdio.h>
 
+
+// Função para inicializar o tabuleiro com zeros
+void iniciar_Tabuleiro (int tabuleiro[10][10]){
+   for (int i = 0; i < 10; i++)
+   {
+      for (int j = 0; j < 10; j++)
+      {
+         tabuleiro[i][j] = 0;
+      }  
+   }
+}
+
+// Função para validar o posicionamento de um navio
+int validar_Navio (int tabuleiro[10][10], int linha, int coluna, int tamanho, int horizontal){
+
+   // Verificar se as coordenadas estão dentro do tabuleiro
+   if (linha < 0 || linha >= 10 || coluna < 0 || coluna >= 10){
+      return 0; // Fora dos limites
+   }
+
+   // Verificar se o navio cabe no tabuleiro
+   if(horizontal){
+      if (coluna + tamanho > 10)
+      {
+         return 0; // Navio ultrapassa o limite horizontal
+      }
+
+      // Verificar sobreposição
+      for(int j = coluna; j < coluna + tamanho; j++){
+         if (tabuleiro[linha][j] != 0)
+         {
+            return 0; // Já existe um navio aqui
+         }  
+      }
+   } else {
+      if (linha + tamanho > 10){
+         return 0; // Navio ultrapassa o limite vertical
+      }
+
+      // Verificar sobreposição
+      for(int i = linha; i < linha + tamanho; i++){
+         if (tabuleiro[i][coluna] != 0)
+         {
+            return 0; // Já existe um navio aqui
+         }
+         
+      }
+   }
+
+   return 1; // Posicionamento válido
+}
+
+// Função para posicionar um navio no tabuleiro
+void posicionar_Navio(int tabuleiro[10][10], int linha, int coluna, int tamanho, int horizontal){
+   if (horizontal)
+   {
+      for (int j = coluna; j < coluna + tamanho; j++){
+         tabuleiro[linha][j] = 3;
+      }
+   } else {
+      for (int i = linha; i < linha + tamanho; i++)
+      {
+         tabuleiro[i][coluna] = 3;
+      }
+      
+   }
+}
+
+// Função para exibir o tabuleiro
+void exibir_Tabuleiro(int tabuleiro[10][10]){
+   printf("\n ");
+   for (int j = 0; j < 10; j++)
+   {
+      printf("1 ");
+   }
+   printf("\n");
+
+   for (int i = 0; i < 10; i++)
+   {
+      printf("1 ");
+      for (int j = 0; j < 10; j++)
+      {
+         printf("%d ", tabuleiro[i][j]);
+      }
+      printf("\n");
+   }
+
+}
+
 int main(){
 
-   // Declarando variáveis e seus tipos
-   char movimentacao;
-   int torre = 0, bispo = 0, rainha = 0;
-
-   // Menu interativo
-   printf("Qual peça você quer movimentar?\n");
-   printf("T - Torre\n");
-   printf("B - Bispo\n");
-   printf("R - Rainha\n");
-   scanf("%c", &movimentacao); // endereça a resposta a variavel movimentacao
+   int tabuleiro[10][10];
+   printf("*** Jogo de Batalha Naval ***\n");
  
+   // Inicializar o tabuleiro
+   iniciar_Tabuleiro(tabuleiro);
 
-   switch (movimentacao) {
+   // Definindo coordenadas iniciais dos navios
+   int navio_Linha1 = 1, navio_Coluna1 = 2; // Navio horizontal
+   int navio_Linha2 = 2, navio_Coluna2 = 3; // Navio vertical
 
-   case 'T':
-   case 't':
-
-      printf("\n***Movimentando torre ***\n"); // Mensagem unica informando qual peça está sendo movimentada
-   // Criação da função de repetição
-   do
-   {
-
-      printf("1 casa para Direita \n");
-
-      torre++;
-   } while (torre <= 4);
-      
-   break;
-
-   case 'B':
-   case 'b':
-
-      printf("\n*** Movimentando bispo ***\n"); // Mensagem unica informando qual peça está sendo movimentada
-      
-   // Criação da função de repetição
-   while (bispo <= 4)
-   {
-   
-      printf("1 casa para Direita \n");
-      printf("1 casa para Cima \n");
-   
-      bispo++;
+   // Validar e posicionar o primeiro navio (horizontal)
+   if(validar_Navio(tabuleiro, navio_Linha1, navio_Coluna1, 3, 1)){
+      posicionar_Navio(tabuleiro, navio_Linha1, navio_Coluna1, 3, 1);
+   } else {
+      printf("Erro: Posicionamento inválido para o navio horizontal!\n");
+      return 1;
    }
 
-   break;
-
-   case 'R':
-   case 'r':
-
-      printf("\n*** Movimentando Rainha ***\n"); // Mensagem unica informando qual peça está sendo movimentada
-
-   // Criação da função de repetição
-   for(rainha = 0; rainha <= 7; rainha++){
-      printf("1 casa para Esquerda \n");
+   // Validar e posicionar o segundo navio (vertical)
+   if (validar_Navio(tabuleiro, navio_Linha2, navio_Coluna2, 3, 0)) {
+      posicionar_Navio(tabuleiro, navio_Linha2, navio_Coluna2, 3, 0);
+   } else {
+      printf("Erro: Posicionamento inválido para o navio vertical!\n");
+      return 1;
    }
 
-   break;
-   
-   default:
-      printf("Valor inválido, tente novamente!\n");
-   break;
-
-
-   }
-
-
+   // Exibir o tabuleiro
+   printf("Tabuleiro com navios posicionados:\n");
+   exibir_Tabuleiro(tabuleiro);
+  
    return 0;
 }
